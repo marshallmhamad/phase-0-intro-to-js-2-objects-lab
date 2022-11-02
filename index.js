50adf80918dbd60b2bd7aa13400407d60ee771b5
marshall <mhamad.marshall@gmail.com>
2022-11-02 21:07:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee  = {
+    name: "marshall",
+    streetAddress: "sulaimany",
+
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let newem = {...employee}
+    newem[key] = value
+    return newem
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+ employee[key] = value
+return employee
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    let nn = {...employee}
+    delete nn[key]
+    return nn
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+delete employee[key]
+return employee
+}
